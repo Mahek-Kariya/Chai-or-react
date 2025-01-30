14069d3e6a00962fd61c6360be80f9047c7c856f
Mahek-Kariya <mahekkariya88@gmail.com>
2025-01-30 12:38:41 +0530

    initialize React project with Vite setup and basic components

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+
+  const username = "chai or code";
+
+  return (
+    <>
+    <Chai />
+    <h1>Chai or react {username}</h1>
+   </>
+  )
+}
+
+export default App
